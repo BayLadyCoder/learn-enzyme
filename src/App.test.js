cead223165d74e4f971cd37d761f15627a7e5885
BayLadyCoder <rachada_32@hotmail.com>
2021-06-19 20:35:55 -0400

    Install Enzyme and adapter, use debug()

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders learn react link", () => {
+  const wrapper = shallow(<App />);
+
+  // Use debug() if you want to see what being rendered in the test
+  console.log(wrapper.debug());
 });
